9ac88ce77ebaf9642d57c8f3f784a364f214c7fc
philfreshman <philipswiezak@gmail.com>
2024-04-25 16:14:48 +0200

    Fix font issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,8 @@
 import Header from "@/components/header"
 import { AlgorithmProvider } from "@/lib/coreContext"
 import { ThemeProvider } from "next-themes"
-import { Inter } from "next/font/google"
 import React from "react"
 import "./globals.css"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
@@ -14,7 +11,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <head>
         <title>ALGO-VISUALIZER</title>
       </head>
-      <body className={inter.className + " h-screen font-mono "} cz-shortcut-listen="false">
+      <body className={"h-screen font-mono"} cz-shortcut-listen="false">
         <ThemeProvider attribute="class" defaultTheme="light">
           <AlgorithmProvider>
             <div className="flex h-full w-full flex-col ">
