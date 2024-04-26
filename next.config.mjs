f2808e89b396f50d112b104de69a7de0081bc1dc
philfreshman <philipswiezak@gmail.com>
2024-04-26 13:27:19 +0200

    Rename favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-  pwa:{
-
-  }
+  reactStrictMode: true
 };
 
 export default nextConfig;
