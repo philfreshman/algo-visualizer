6dd7170ca725fad7214080b4cc808e4c9e75e182
philfreshman <philipswiezak@gmail.com>
2024-05-12 22:10:24 +0200

    Upgrade dependencies

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -2,5 +2,12 @@
 export const cols = 41
 export const rows = 31
 
-export const searchAlgorithms: AlgorithmTypes = { DFS: "Depth first search", BFS: "Breath first search" }
-export const mazeAlgorithms: AlgorithmTypes = { LABYRINTH: "Labyrinth", CUSTOM: "Custom", SPICED: "Spiced" }
+export const searchAlgorithms: AlgorithmTypes = {
+  DFS: "Depth first search",
+  BFS: "Breath first search",
+}
+export const mazeAlgorithms: AlgorithmTypes = {
+  LABYRINTH: "Labyrinth",
+  CUSTOM: "Custom",
+  SPICED: "Spiced",
+}
