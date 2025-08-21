5c67dbcdb45e2a5dfe949425517098baef712a05
philfreshman <philipswiezak@gmail.com>
2025-08-21 09:18:12 +0200

    Upgrade dependencies and refine `useEffect` dependency array in `run-menu.tsx`

--- src/components/nav-menu/run-menu.tsx
+++ src/components/nav-menu/run-menu.tsx
@@ -32,7 +32,7 @@ export default function RunMenu() {
 
     useEffect(() => {
         if (isCompleted) setIsRunning(false)
-    }, [isCompleted])
+    }, [isCompleted, setIsRunning])
 
     keyPress('x', resetAll)
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,7 @@ const config = {
             },
         },
     },
+    plugins: [require("tailwindcss-animate")]
 } satisfies Config
 
 export default config
