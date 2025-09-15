fcfa1d592f06b1199a8d80a83bd71f5d3aeb4699
philfreshman <philipswiezak@gmail.com>
2025-09-15 09:27:28 +0200

    Adjust github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     output: 'export',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
+    distDir: 'out',
+    images: {
+        unoptimized: true
+    },
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/algo-visualizer' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/algo-visualizer' : '',
     webpack: (config) => {
         // Add support for WASM
         config.experiments = {
